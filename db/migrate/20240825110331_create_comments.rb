50b66d722e744c68b9addce0615064f33fee2d01
firecomenntdo <shikaneko6@gmail.com>
2024-08-27 14:44:43 +0900

    db comments table修正

--- db/migrate/20240825110331_create_comments.rb
+++ db/migrate/20240825110331_create_comments.rb
@@ -1,6 +1,6 @@
-class CreateCommtents < ActiveRecord::Migration[7.0]
+class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :commtents do |t|
+    create_table :comments do |t|
       t.text       :content,                null: false
       t.references :prototype,            null: false, foreign_key: true
       t.references :user,                 null: false, foreign_key: true
